e08123435765c1e4eb6933a413790b9074dfe263
jordan lafontant <(jordan.lafontant@bison.howard.edu>
2018-04-27 19:19:46 -0400

    partial tinder files

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Jordan on 4/17/18.
+//  Copyright © 2018 Jordan All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
